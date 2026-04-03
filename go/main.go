29f2654b7d794df8f10fc16f1d4d6d2ae783ba2f
Nick Song <nick.song@footway.com>
2026-04-03 23:52:31 +0200

    initial

--- go/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/nicksenap/grove/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- src/grove/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow running Grove with: python -m grove."""
-
-from grove.cli import app
-
-app()
